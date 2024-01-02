3c6d5d895490125b1f8bfb1f0250b0acac4e4bc2
Leonard Laput <online.lorno@gmail.com>
2024-01-02 17:14:26 +0800

    ✨ Setup font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import withMT from '@material-tailwind/react/utils/withMT'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -7,14 +8,26 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-montserrat)', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        main: '#252B42',
+        primary: '#23A6F0',
+        secondary: {
+          green: '#23856D',
+          yellow: '#F3CD03',
+          gray: '#737373',
+        },
+        danger: '#E74040',
+        success: '#2DC071',
+        muted: '#BDBDBD',
+        disabled: '#8EC2F2',
+        'light-gray': '#FAFAFA',
       },
     },
   },
   plugins: [],
 }
-export default config
+export default withMT(config)
