6bc53120c0acbfc2eeeab8638f4517bbeeac7993
Leonard Laput <online.lorno@gmail.com>
2024-01-03 13:44:53 +0800

    💄 Update brandlist styles

--- src/components/app/products/[id]/BrandList.tsx
+++ src/components/app/products/[id]/BrandList.tsx
@@ -12,13 +12,13 @@ import {
 export function BrandListSection() {
   return (
     <section className="mx-auto max-w-screen-1.5xl px-12 py-[50px] lg:px-0">
-      <div className="flex flex-col flex-wrap items-center justify-between gap-[30px] md:flex-row">
-        <FaHooli className="h-[100px] w-[100px]" />
-        <FaLyft className="h-[100px] w-[100px]" />
-        <FaPiedPiperHat className="h-[100px] w-[100px]" />
-        <FaStripe className="h-[100px] w-[100px]" />
-        <FaAws className="h-[100px] w-[100px]" />
-        <FaRedditAlien className="h-[100px] w-[100px]" />
+      <div className="flex flex-col flex-wrap items-center justify-between gap-[30px] children:h-[100px] children:w-[100px] children:text-secondary-gray md:flex-row">
+        <FaHooli />
+        <FaLyft />
+        <FaPiedPiperHat />
+        <FaStripe />
+        <FaAws />
+        <FaRedditAlien />
       </div>
     </section>
   )
