eb90aa3c0a7192c0351543d3dc992e9a9012ab2e
Leonard Laput <online.lorno@gmail.com>
2024-01-03 13:23:42 +0800

    ✨ Best seller products section

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { BreadCrumbsSection } from '@/components/BreadCrumbs'
+import { BestSellerProductsSection } from '@/components/app/products/[id]/BestSellerProducts'
 import { ProductDetailsSection } from '@/components/app/products/[id]/ProductDetails'
 import { ProductInformationSection } from '@/components/app/products/[id]/ProductInformation'
 import { SITE_NAME } from '@/constants/site'
 import { SITE_BREADCRUMBS } from '@/constants/site'
-import { Product } from '@/lib/types'
+import { Product, ProductsResponse } from '@/lib/types'
 import { fetchServer } from '@/lib/utils'
 import { Metadata, ResolvingMetadata } from 'next'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
@@ -14,6 +15,10 @@ type Props = {
 
 async function getProduct(id: string): Promise<Product> {
   return await fetchServer(`/products/${id}`)
+}
+
+async function getRelatedProducts(category: string): Promise<ProductsResponse> {
+  return await fetchServer(`/products/category/${category}`)
 }
 
 export async function generateMetadata(
@@ -45,12 +50,14 @@ export default async function ProductPage({ params }: Props) {
       link: `/products/${product.id}`,
     },
   ]
+  const relatedProductsResponse = await getRelatedProducts(product.category)
 
   return (
     <>
       <BreadCrumbsSection breadCrumbs={breadCrumbs} />
       <ProductDetailsSection product={product} />
       <ProductInformationSection product={product} />
+      <BestSellerProductsSection products={relatedProductsResponse.products} />
     </>
   )
 }
--- src/components/ProductListGrid.tsx
+++ src/components/ProductListGrid.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function ProductListGrid({ products }: Props) {
   return (
-    <div className="flex flex-wrap justify-center gap-x-[30px] gap-y-[30px] p-6 md:gap-y-[15px]">
+    <div className="flex flex-wrap justify-center gap-x-[30px] gap-y-[30px] md:gap-y-[15px]">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
